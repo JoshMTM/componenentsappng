94b28a0d18028ac32c45ba4d31b223c5dc3160bc
Joshua Mitima <joshuamitima@joshuas-mbp.home>
2022-10-04 16:02:18 +0200

    table comoponents and styling

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+data = [
+  { name: 'James', job: 'Designer', age: 24, employed: true }, 
+  { name: 'Jill', job: 'Engineer', age: 26, employed: false  },
+  { name: 'Elyse', job: 'Enginner', age: 25, employed: true  }
+];
+
+headers = [
+  { key: 'name', label: 'Name' },
+  { key: 'age', label: 'Age' },
+  { key: 'job', label: 'Job' },
+  { key: 'employed', label: 'Has a Job?'}
+];
+
   constructor() { }
 
   ngOnInit(): void {
